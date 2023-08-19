9f454b6444470bb8de397f215ee6a296172fee17
khushalikarlia15 <khushalikarelia15@gmail.com>
2023-08-19 11:18:37 +0530

    first commit

---
+++ Models/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeCrudMVC.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required (ErrorMessage ="* Name cannot be empty")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "* City cannot be empty")]
+        public string City { get; set; }
+        [Required(ErrorMessage = "* State cannot be empty")]
+        public string State { get; set; }
+        [Required]
+        public decimal? Salary { get; set; }
+
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmployeeCrudMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
